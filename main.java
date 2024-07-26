f95047a677100bf3aa0b02ca0d2eb4b19a2e050d
Hirak Jyoti Das <dhirakjyoti11@gmail.com>
2024-07-26 16:28:33 +0530

    commited few files

---
+++ main.java
@@ -0,0 +1,13 @@
+// every files that ends with .java is a class 
+// so the class name is basically the file name and vice-versa
+// class name should be started with capital letter
+// public class means the class can be accessed from another classes
+// main function --> public static void main(String[] args)
+
+
+
+public class main{
+	public static void main(String args[]){ 
+		System.out.println("Hello ");
+	}
+}
---
+++ max-min.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class max-min
+{
+	public static void main(string args[])
+	{
+		int n=args.length,sum=0;
+		System.out.println("The number of values i have given is ")
+	}
+}
